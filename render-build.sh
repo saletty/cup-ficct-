d1b26bb91de137c382c34cec0dbd0433282d4ca8
saletty <salet.ortega03@gmail.com>
2026-05-31 18:23:00 -0400

    Add render build script

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+composer install --no-dev --optimize-autoloader
+npm install
+npm run build
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
