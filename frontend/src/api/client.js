1598e690ab2a383cdc9a400a8151c188ca80f97b
saletty <salet.ortega03@gmail.com>
2026-05-31 22:12:12 -0400

    Debug API URL in production

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1';
+
+console.log('API URL:', API_URL);
+
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://127.0.0.1:8000/api/v1'
+  baseURL: API_URL
 });
 
 client.interceptors.request.use((config) => {
