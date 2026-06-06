21abb4e6c31cb21f615b7d6e7da3574b7673cff0
saletty <salet.ortega03@gmail.com>
2026-06-06 01:10:09 -0400

    fix: remove duplicate CORS headers - single source via Laravel middleware

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://cup-ficct-mocha.vercel.app',
+        'https://cup-ficct-leps9wjz6-salettys-projects.vercel.app',
+        'http://localhost:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
