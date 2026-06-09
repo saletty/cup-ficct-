4416a5941600a5b6ff4a50b72ea4b6fda5b5a7f5
saletty <salet.ortega03@gmail.com>
2026-06-09 14:51:55 -0400

    fix: registrar transport de Brevo API en AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,21 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoApiTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Mail::extend('brevo', function () {
+            $factory = new BrevoApiTransportFactory(null, HttpClient::create());
+            return $factory->create(new Dsn('brevo+api', 'default', config('services.brevo.key')));
+        });
+    }
 }
